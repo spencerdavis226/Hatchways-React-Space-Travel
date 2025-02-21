2b59f7d2a8af841c2e96be9a3796b69ec6845ac1
Spencer Davis <spencerdavis226@gmail.com>
2025-02-21 12:48:57 -0500

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import styles from "./App.module.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
-function App ()
-{
+// PAGES
+import HomePage from './pages/HomePage';
+import SpacecraftsPage from './pages/SpacecraftsPage';
+import SpacecraftDetailPage from './pages/SpacecraftDetailPage';
+import ConstructionPage from './pages/ConstructionPage';
+import PlanetsPage from './pages/PlanetsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+// STYLES
+import styles from './App.module.css';
+
+function App() {
   return (
-    <>
-      <h1>Space Travel</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="spacecrafts" element={<SpacecraftsPage />} />
+        <Route path="spacecrafts/:id" element={<SpacecraftDetailPage />} />
+        <Route path="construct" element={<ConstructionPage />} />
+        <Route path="planets" element={<PlanetsPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
